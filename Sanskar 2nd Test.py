494496ff1bb313a684080511b02955c0186e04ca
Sanskar Yadav <sanskaryadav287@gmail.com>
2025-11-10 01:41:22 -0500

    Add files via upload

---
+++ Sanskar 2nd Test.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## 1.
+
+# In[1]:
+
+
+x=15
+y=48
+x>10 and y<50
+
+
+# ##  2.
+
+# In[10]:
+
+
+age=18
+age2=18
+age<=age2
+
+
+# ## 3.
+# 
+
+# In[5]:
+
+
+x="true"
+y="false"
+x is y
+x is x
+
+
+# ## 4.
+
+# In[7]:
+
+
+a=5
+b=2
+a>10 or b<8
+
+
+# ## 5.
+
+# In[13]:
+
+
+x=62
+x/=3
+x
+
+
+# ## 6.
+
+# In[14]:
+
+
+password="Admin1"
+print("Access Granted")
+
+
+# In[16]:
+
+
+len(password)
+
+
+# ## 7.
+
+# In[18]:
+
+
+a=7
+b=12
+a<10 and b>10
+
+
+# In[19]:
+
+
+x=8
+y=16
+x<=y
+
+
+# ## 8. 
+
+# In[20]:
+
+
+s=5
+r=10
+s!=r
+
